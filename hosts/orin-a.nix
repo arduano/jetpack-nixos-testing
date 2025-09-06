42daf1ccd3b2a7adb5d08062828bf22bde761501
arduano <leonid.shchurov@gmail.com>
2025-09-06 18:38:12 +1000

    Give "known good generation" script to orin A

--- hosts/orin-a.nix
+++ hosts/orin-a.nix
@@ -1,2 +1,15 @@
 { ... }: {
+    environment.systemPackages = [
+    (pkgs.writeShellScriptBin "status" ''
+      echo "Known good generation"
+    '')
+  ];
+
+  # # Exit the initrd scripts early -> kernel panics "Attempted to kill init!"
+  # boot.initrd.postMountCommands = lib.mkAfter ''
+  #   echo "Intentional test failure: exiting initrd" >&2
+  #   exit 1
+  # '';
+  # # (Optional) reboot quickly after panic
+  # boot.kernelParams = lib.mkAfter [ "panic=5" ];
 }
